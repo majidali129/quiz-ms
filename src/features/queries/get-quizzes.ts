2b99cc414c9895952e6046248ccdf630ffec8f3b
majidali129 <itsmajidali129@gmail.com>
2025-05-12 02:04:55 +0500

    fix: unused-var

--- src/features/queries/get-quizzes.ts
+++ src/features/queries/get-quizzes.ts
@@ -6,7 +6,7 @@ export const getQuizzes = async (teacherId?: string): Promise<QuizType[]> => {
   await connectDB();
 
   if (teacherId) {
-    return JSON.parse(JSON.stringify(await Quiz.find().populate({ path: "createdBy", select: "_id userName picture" }).populate({ path: "course", select: "_id title" }).lean().exec()));
+    return JSON.parse(JSON.stringify(await Quiz.find({ createdBy: teacherId }).populate({ path: "createdBy", select: "_id userName picture" }).populate({ path: "course", select: "_id title" }).lean().exec()));
   }
 
   return JSON.parse(JSON.stringify(await Quiz.find().populate({ path: "createdBy", select: "_id userName picture" }).populate({ path: "course", select: "_id title" }).lean().exec()));
